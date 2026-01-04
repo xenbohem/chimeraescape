50280f0c63c92133b5e8704c0a8462659dcebed3
xenbohem <xenv@mailfence.com>
2026-01-03 19:06:23 -0800

    Update script.js

--- FORUM-LOGIN/script.js
+++ FORUM-LOGIN/script.js
@@ -1,7 +1,7 @@
 // =======================
 // SUPABASE SETUP
 // =======================
-const supabaseUrl = "YOUR_SUPABASE_URL";
+const supabaseUrl = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhldGNqcG94ZHlyemZjaW9ub2hxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc0MzY3MzEsImV4cCI6MjA4MzAxMjczMX0.658Q1QUpw11rnNCkNyGKuk4UJ9ERwKZqVIOTKDUxODs";
 const supabaseKey = "sb_publishable_tgYPzSe_w9fyGGVZeJQ-Hg_yWZLeIW9";
 const supabase = supabaseJs.createClient(supabaseUrl, supabaseKey);
 
